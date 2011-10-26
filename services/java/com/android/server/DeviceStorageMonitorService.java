986d594f30b7f2f65451f8a7afc2cb9ec8f21c3a
Josh Stone <cuviper@gmail.com>
2011-10-25 22:52:20 -0700

    Monitor device storage directly in /data/data
    The DeviceStorageMonitorService watches over /data, and when things get
    full, starts deleting app cached files.  The app caches are actually
    under /data/data, which on DINC is its own mount.  This means the
    monitor did not notice when app cache grew too big, as it only saw the
    size of /data proper.
    
    This patch makes the monitor look at /data/data instead.  For DINC, this
    means it can properly manage app cache, but it won't notice if the upper
    /data partition is getting full.  That's not as common as /data/data
    filling up though.  For the Samsung devices that mount a /datadata and
    then symlink to /data/data, it should also work, as statfs dereferences
    links first.  For all other devices with only /data, reading statfs on
    /data/data still yields the total/free sizes of the /data partition.
    
    Change-Id: I118a85a04c3a257d54d40ce7802426b4e0fe3e87
    
    Conflicts:
    	services/java/com/android/server/DeviceStorageMonitorService.java

--- services/java/com/android/server/DeviceStorageMonitorService.java
+++ services/java/com/android/server/DeviceStorageMonitorService.java
@@ -81,9 +81,9 @@ public class DeviceStorageMonitorService extends Binder {
     private static final int DEFAULT_FREE_STORAGE_LOG_INTERVAL_IN_MINUTES = 12*60; //in minutes
     private static final long DEFAULT_DISK_FREE_CHANGE_REPORTING_THRESHOLD = 2 * 1024 * 1024; // 2MB
     private static final long DEFAULT_CHECK_INTERVAL = MONITOR_INTERVAL*60*1000;
-
-    private long mFreeMem;  // on /data
-    private long mFreeMemAfterLastCacheClear;  // on /data
+    private static final int DEFAULT_FULL_THRESHOLD_BYTES = 1024*1024; // 1MB
+    private long mFreeMem;  // on /data/data
+    private long mFreeMemAfterLastCacheClear;  // on /data/data
     private long mLastReportedFreeMem;
     private long mLastReportedFreeMemTime;
     private boolean mLowMemFlag=false;
@@ -91,14 +91,13 @@ public class DeviceStorageMonitorService extends Binder {
     private Context mContext;
     private Context mUiContext;
     private ContentResolver mContentResolver;
-    private long mTotalMemory;  // on /data
+    private long mTotalMemory;  // on /data/data
     private StatFs mDataFileStats;
     private StatFs mSystemFileStats;
     private StatFs mCacheFileStats;
-    private static final File DATA_PATH = Environment.getDataDirectory();
-    private static final File SYSTEM_PATH = Environment.getRootDirectory();
-    private static final File CACHE_PATH = Environment.getDownloadCacheDirectory();
-
+    private static final String DATA_PATH = "/data/data"; // might not be the same fs as /data
+    private static final String SYSTEM_PATH = "/system";
+    private static final String CACHE_PATH = "/cache";
     private long mThreadStartTime = -1;
     private boolean mClearSucceeded = false;
     private boolean mClearingCache;
