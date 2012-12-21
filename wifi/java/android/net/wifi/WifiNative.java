7343fb2044a66fa455158a9e095fbeb1926a19c4
Steve Kondik <shade@chemlab.org>
2012-12-20 20:40:52 -0800

    wifi: Set the mode at native layer
     * Set normal/AP mode so that the HAL can specify correct module
       arguments when loading the driver.
    
    Change-Id: I6eee2cc53b5f1e70306e252e52c909833f220515
    
    Conflicts:
    	wifi/java/android/net/wifi/WifiNative.java
    	wifi/java/android/net/wifi/WifiStateMachine.java

--- wifi/java/android/net/wifi/WifiNative.java
+++ wifi/java/android/net/wifi/WifiNative.java
@@ -798,13 +798,5 @@ public class WifiNative {
         return doBooleanCommand("P2P_SERV_DISC_CANCEL_REQ " + id);
     }
 
-    /* Set the current mode of miracast operation.
-     *  0 = disabled
-     *  1 = operating as source
-     *  2 = operating as sink
-     */
-    public void setMiracastMode(int mode) {
-        // Note: optional feature on the driver. It is ok for this to fail.
-        doBooleanCommand("DRIVER MIRACAST " + mode);
-    }
+    public native static boolean setMode(int mode);
 }
