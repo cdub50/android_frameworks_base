421df699ab5a7583e0c49304717720bed8d66638
Daniel MM <daxx4182@gmail.com>
2013-06-15 14:38:53 +0800

    HALO: Circle, Bubble, text colors
    Conflicts:
    	packages/SystemUI/res/drawable-hdpi/halo_bg.png
    	packages/SystemUI/res/drawable-nodpi/bubble_black_l.9.png
    	packages/SystemUI/res/drawable-nodpi/bubble_black_r.9.png
    	packages/SystemUI/res/drawable-xhdpi/halo_bg.png
    	packages/SystemUI/res/layout/halo_bubble.xml
    	packages/SystemUI/res/layout/halo_speech.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/halo/HaloProperties.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2742,6 +2742,36 @@ public final class Settings {
          */
         public static final String PIE_SOFTKEYBOARD_IS_SHOWING = "pie_softkeyboard_is_showing";
 
+ 	/*
+         * HALO being minimal.
+         * @hide
+         */
+        public static final String HALO_STYLE = "halo_style";
+
+        /**
+         * HALO speech bubble color
+         * @hide
+         */
+        public static final String HALO_BUBBLE_COLOR = "halo_bubble_color";
+
+        /**
+         * HALO speech bubble text color
+         * @hide
+         */
+        public static final String HALO_BUBBLE_TEXT_COLOR = "halo_bubble_text_color";
+
+        /**
+         * HALO circle bg color
+         * @hide
+         */
+        public static final String HALO_CIRCLE_COLOR = "halo_circle_color";
+
+        /**
+         * AutoHide CombinedBar on tablets.
+         * @hide
+         */
+        public static final String COMBINED_BAR_AUTO_HIDE = "combined_bar_auto_hide";
+
         /**
          * Pie allow to adjust triggers if IME is showing (0 or 1)
          * @hide
