3bb2f0ac853cbbf973491e77dce77655f3e51be7
Danny Baumann <dannybaumann@web.de>
2013-08-14 09:48:05 +0200

    Lockscreen targets: more unification
    Don't hardcode pixel sizes, and use a shared method to derive drawable
    inset.
    
    Change-Id: I2adf9230c5aa8efadddd713c435b37af24e23115
    
    Conflicts:
    	core/res/res/values/symbols.xml

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardSelectorView.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardSelectorView.java
@@ -25,7 +25,6 @@ import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
