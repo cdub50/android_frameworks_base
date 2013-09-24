07bc093b2dd20c40751bc7b2d2b42c0cb2aadfa5
cristianomatos <cristianobmatos@gmail.com>
2013-09-24 07:28:46 -0300

    AOKP custom system animations (1/2)
    Ported from here: http://gerrit.aokp.co/#/c/12304/
    
    Thanks to Steve Spear
    
    Change-Id: I38bc7b83edaff3c455f0dfe3ea5ede54a20ae07c
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/cm_strings.xml
    	core/res/res/values/symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4139,7 +4139,7 @@ public final class Settings {
          * 8 == Translate (Right)
          * @hide
          */
-         public static final String LISTVIEW_ANIMATION = "listview_animation";
+        public static final String LISTVIEW_ANIMATION = "listview_animation";
 
         /**
          * ListView Interpolators
@@ -4153,7 +4153,26 @@ public final class Settings {
          * 7 == bounce_interpolator
          * @hide
          */
-         public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
+        public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
+
+	    /**
+         * Custom System Animations
+         * @hide
+         */
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
+                "activity_open",
+                "activity_close",
+                "task_open",
+                "task_close",
+                "task_to_front",
+                "task_to_back",
+                "wallpaper_open",
+                "wallpaper_close",
+                "wallpaper_intra_open",
+                "wallpaper_intra_close",
+        };
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+        public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
 
         /**
          * Whether FastPowerOn is enabled
