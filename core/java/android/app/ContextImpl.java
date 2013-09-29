74e7687ea09b3aebf55afebe26312d32a74afa6a
liquid0624 <rjbirnesser@gmail.com>
2013-09-29 15:24:27 -0400

    Fix missing ap ops service derp

--- core/java/android/app/ContextImpl.java
+++ core/java/android/app/ContextImpl.java
@@ -530,6 +530,13 @@ class ContextImpl extends Context {
                 return new UserManager(ctx, service);
             }});
 
+        registerService(APP_OPS_SERVICE, new ServiceFetcher() {
+            public Object createService(ContextImpl ctx) {
+                IBinder b = ServiceManager.getService(APP_OPS_SERVICE);
+                IAppOpsService service = IAppOpsService.Stub.asInterface(b);
+                return new AppOpsManager(ctx, service);
+            }});
+
         registerService(PROFILE_SERVICE, new ServiceFetcher() {
                 public Object createService(ContextImpl ctx) {
                     final Context outerContext = ctx.getOuterContext();
