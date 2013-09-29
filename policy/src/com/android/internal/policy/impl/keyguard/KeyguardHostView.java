SlimRoms/frameworks_base

Do not show camera widget by default
Change-Id: I157e021fb7a70d76b276c7e17a098f9a16dfc494

policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
@@ -1051,7 +1051,7 @@ public class KeyguardHostView extends KeyguardViewBase {
         // inflate system-provided camera?
         if (!mSafeModeEnabled && !cameraDisabledByDpm()
                 && Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.KG_CAMERA_WIDGET, 1) == 1) {
+                Settings.System.KG_CAMERA_WIDGET, 0) == 1) {
             View cameraWidget =
                     CameraWidgetFrame.create(mContext, mCameraWidgetCallbacks, mActivityLauncher);
             if (cameraWidget != null) {

cdub50/android_frameworks_base

Conflicts:
	policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java

policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
@@ -1214,7 +1214,7 @@ public class KeyguardHostView extends KeyguardViewBase {
         // inflate system-provided camera?
         if (!mSafeModeEnabled && !cameraDisabledByDpm()
                 && Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.LOCKSCREEN_CAMERA_WIDGET, 1) == 1) {
+                Settings.System.LOCKSCREEN_CAMERA_WIDGET, 0) == 1) {
             View cameraWidget =
                     CameraWidgetFrame.create(mContext, mCameraWidgetCallbacks, mActivityLauncher);
             if (cameraWidget != null) {