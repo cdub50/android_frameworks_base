d06976d34e433083dea628485e819270e01e564a
liquid0624 <rjbirnesser@gmail.com>
2013-10-09 18:35:39 -0400

    Cleanup more services whitespace

--- services/java/com/android/server/DeviceStorageMonitorService.java
+++ services/java/com/android/server/DeviceStorageMonitorService.java
@@ -81,6 +81,7 @@ public class DeviceStorageMonitorService extends Binder {
     private static final int DEFAULT_FREE_STORAGE_LOG_INTERVAL_IN_MINUTES = 12*60; //in minutes
     private static final long DEFAULT_DISK_FREE_CHANGE_REPORTING_THRESHOLD = 2 * 1024 * 1024; // 2MB
     private static final long DEFAULT_CHECK_INTERVAL = MONITOR_INTERVAL*60*1000;
+
     private long mFreeMem;  // on /data/data
     private long mFreeMemAfterLastCacheClear;  // on /data/data
     private long mLastReportedFreeMem;
@@ -88,12 +89,13 @@ public class DeviceStorageMonitorService extends Binder {
     private boolean mLowMemFlag=false;
     private boolean mMemFullFlag=false;
     private Context mContext;
-    private Context mUiContext;
     private ContentResolver mResolver;
+    private Context mUiContext;
     private long mTotalMemory;  // on /data/data
     private StatFs mDataFileStats;
     private StatFs mSystemFileStats;
     private StatFs mCacheFileStats;
+
     private static final File DATA_PATH = Environment.getDataDirectory();
     private static final File SYSTEM_PATH = Environment.getRootDirectory();
     private static final File CACHE_PATH = Environment.getDownloadCacheDirectory();
